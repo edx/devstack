17ba326f5afe7477a777dbf01b926b9bcdd5567e
Tim McCormack <tmccormack@edx.org>
2025-03-05 20:24:08 +0000

    fixup! Include name of profile file

--- py_configuration_files/codejail.py
+++ py_configuration_files/codejail.py
@@ -12,8 +12,8 @@ ALLOWED_HOSTS = [
 CODEJAIL_ENABLED = True
 
 CODE_JAIL = {
-    # These values are coordinated with the Dockerfile and the AppArmor profile,
-    # both in edx/public-dockerfiles.
+    # These values are coordinated with the Dockerfile and the AppArmor
+    # profile (openedx_codejail_service.profile) both in edx/public-dockerfiles.
     'python_bin': '/sandbox/venv/bin/python',
     'user': 'sandbox',
 
